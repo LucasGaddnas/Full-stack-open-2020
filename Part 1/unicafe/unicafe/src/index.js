288fb8f24adeea4a8c8d33fd6cc557bb9855a399
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-06-16 21:33:43 +0300

    Commit exercise 1.7

--- Part 1/unicafe/unicafe/src/index.js
+++ Part 1/unicafe/unicafe/src/index.js
@@ -2,14 +2,31 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+    setAverage((good + 1 - bad) / (all + 1))
+    setPositive((good + 1) / (all + 1) * 100)
+  }
+  const handleNeutral = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+    setAverage((good - bad) / (all + 1))
+    setPositive((good) / (all + 1) * 100)
+  }
+  const handleBad = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+    setAverage((good - bad - 1) / (all + 1))
+    setPositive((good) / (all + 1) * 100)
+  }
 
   return (
     <div>
@@ -27,6 +44,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
     </div>
   )
 }
