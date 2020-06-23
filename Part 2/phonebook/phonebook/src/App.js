9f5931553c63d1803f1d4c6877ce4b481e819c17
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-06-23 21:36:24 +0300

    Commit exercise 2.6

---
+++ Part 2/phonebook/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [ persons, setPersons ] = useState([
+        { name: 'Arto Hellas' }
+    ]) 
+    const [ newName, setNewName ] = useState('')
+
+    const addName = (event) => {
+        event.preventDefault()
+        const person = { name: newName }
+        setPersons(persons.concat(person))
+        setNewName('')
+    }
+    const handleNameCHange = (event) => setNewName(event.target.value)
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+                <form onSubmit={addName}>
+                    <div>
+                        name:
+                        <input
+                            value={newName} onChange={handleNameCHange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">add</button>
+                    </div>
+                </form>
+            <h2>Numbers</h2>
+            <div>
+                {persons.map(person =>
+                    <div key={person.name}>{person.name}</div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default App
