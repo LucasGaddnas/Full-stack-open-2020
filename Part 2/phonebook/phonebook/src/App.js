92c6838b3b91f50fde74376a81bcccfab2418ba2
Lucas Gäddnäs <55992317+LucasGaddnas@users.noreply.github.com>
2020-06-27 11:41:58 +0300

    Commit exercise 2.11

--- Part 2/phonebook/phonebook/src/App.js
+++ Part 2/phonebook/phonebook/src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/persons'
 import Form from './components/form'
 import Filter from './components/filter'
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-1234567' }
-    ]) 
+    const [ persons, setPersons ] = useState([]) 
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ filter, setFilter ] = useState('')
+
+    const hook = () => {
+        axios.get('http://localhost:3001/persons')
+        .then(response => {setPersons(response.data)})
+    }
+    useEffect(hook, [])
 
     const addName = (event) => {
         event.preventDefault()
